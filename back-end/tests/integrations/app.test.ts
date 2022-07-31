4047afdf0afe2dd7207ed0dedb56b93b1ed3edc2
heusyfelype <felype.heusy@gmail.com>
2022-07-31 16:54:46 -0300

    feat: teste de integração do get recommendation concluído)

--- back-end/tests/integrations/app.test.ts
+++ back-end/tests/integrations/app.test.ts
@@ -1,14 +1,33 @@
 import app from "../../src/app.js";
-import supertest from "supertest";
+import { jest } from "@jest/globals";
+import supertest, { Request } from "supertest";
+import { prisma } from "./../../src/database.js"
+import * as factory from "./factories/recomendationsFactory.js";
 
-describe("POST /recommendations", () =>{
-    it("given a name and a url, it should return created", async () =>{
-        const body = {
-            "name": "Adele - I Drink Wine (Live at The BRIT Awards 2022)",
-            "youtubeLink": "https://youtu.be/LwXQ7WUh-D0?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbAVMLwXQ7WUh-D0"
-        }
+beforeEach(async () => {
+    await prisma.$executeRaw`TRUNCATE TABLE recommendations`
+})
+
+describe("POST /recommendations", () => {
+    it("given a name and a url, it should return created", async () => {
+        const body = factory.arrCreateRecommendations[0];
         const firstTry = await supertest(app).post("/recommendations").send(body);
-        expect(firstTry.status).toEqual(201)
+        expect(firstTry.status).toEqual(201);
     })
+    it("given the same name and a url two times, it should return error 409", async () => {
+        const body = factory.arrCreateRecommendations[0];
+        const firstTry = await supertest(app).post("/recommendations").send(body);
+        const SecondTry = await supertest(app).post("/recommendations").send(body);
 
+        expect(SecondTry.status).toEqual(409);
+    })
+})
+
+describe("GET /recomendations", () => {
+    it("it should return 10 videos", async () => {
+        factory.createAllRecommendations();
+        const response = await supertest(app).get("/recommendations")
+        console.log("RESPONSNE::::::::::: ", response.body)
+        expect(response.body).toHaveLength(10);
+    })
 })
---
+++ back-end/tests/integrations/factories/recomendationsFactory.ts
@@ -0,0 +1,43 @@
+import supertest from "supertest"
+import app from "../../../src/app.js"
+import { prisma } from "../../../src/database.js"
+
+export const arrCreateRecommendations = [{
+    "name": "Rammstein - Deutschland (Official Video)",
+    "youtubeLink": "https://youtu.be/NeQM1c-XCDc"
+}, {
+    "name": "Adele - I Drink Wine (Live at The BRIT Awards 2022)",
+    "youtubeLink": "https://youtu.be/LwXQ7WUh-D0?list=RDGMEMQ1dJ7wXfLlqCjwV0xfSNbAVMLwXQ7WUh-D0"
+}, {
+    "name": "Daft Punk - Instant Crush (Video) ft. Julian Casablancas",
+    "youtubeLink": "https://youtu.be/a5uQMwRMHcs?list=RDB4-OxOmsqR0"
+}, {
+    "name": "Stromae - Fils de joie (Official Music Video)",
+    "youtubeLink": "https://youtu.be/M7Z2tgJo8Hg?list=RDB4-OxOmsqR0"
+}, {
+    "name": "MGMT - Kids (Official HD Video)",
+    "youtubeLink": "https://youtu.be/fe4EK4HSPkI?list=RDB4-OxOmsqR0"
+}, {
+    "name": "M83 - 'Wait' (Official Video)",
+    "youtubeLink": "https://youtu.be/lAwYodrBr2Q"
+}, {
+    "name": "Pink Floyd Another Brick In The Wall (HQ)",
+    "youtubeLink": "https://youtu.be/bZwxTX2pWmw"
+}, {
+    "name": "David Gilmour - Rattle That Lock (Official Music Video)",
+    "youtubeLink": "https://youtu.be/L1v7hXEQhsQ"
+}, {
+    "name": "The Killers - Mr. Brightside (Official Music Video)",
+    "youtubeLink": "https://youtu.be/gGdGFtwCNBE"
+}, {
+    "name": "Muse - Madness",
+    "youtubeLink": "https://youtu.be/Ek0SgwWmF9w"
+}, {
+    "name": "Empire Of The Sun - Way To Go (Official Video)",
+    "youtubeLink": "https://youtu.be/xg9ebVTL9yE?list=RDMM"
+}
+]
+
+export async function createAllRecommendations() {
+    return await prisma.recommendation.createMany({ data: arrCreateRecommendations })
+}
